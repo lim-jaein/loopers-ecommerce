9b1323908c03d6c1cd62e1d4111b18b11bbe214b
im-jaein <imjaein00@gmail.com>
2025-11-14 16:30:42 +0900

    feat : 브랜드 생성/조회 구현 및 테스트코드 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
@@ -0,0 +1,44 @@
+package com.loopers.domain.brand;
+
+import com.loopers.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "brands")
+public class Brand extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    protected Brand() {}
+
+    private Brand(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public static Brand create(String name, String description) {
+        validateName(name);
+        validateDescription(description);
+        return new Brand(name, description);
+    }
+
+    private static void validateDescription(String description) {
+        if(description == null) {
+            throw new IllegalArgumentException("브랜드 설명이 null일 수 없습니다.");
+        }
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("브랜드명이 입력되지 않았습니다.");
+        }
+    }
+}
